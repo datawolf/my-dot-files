50cd8401e0e9d69e98a5777990f2f03750766c36
Wang Long <wanglong@laoqinren.net>
2018-06-23 23:00:24 +0800

    add install.sh
    Signed-off-by: Wang Long <wanglong@laoqinren.net>

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+mkdir -p ~/bin
+cp -rf bin/* ~/bin/
+cp -rf .bashrc ~/
+cp -rf .gitconfig ~/
+cp -rf .tmux.conf ~/
+cp -rf .vim ~/
+cp -rf .vimrc ~/
